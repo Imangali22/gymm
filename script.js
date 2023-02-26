73f411f7f52e11c01c808a232a056691c9235238
Imangali22 <126374131+Imangali22@users.noreply.github.com>
2023-02-26 11:31:47 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function openReg(){
+    document.getElementsByClassName(".container").className = "container2";
+}
+
+let button = document.querySelector('.profileBtn')
+button.addEventListener('click', openReg)
