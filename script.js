6193b0cd9b15df1048ff91a2ee9a9ea078302a70
Imangali22 <126374131+Imangali22@users.noreply.github.com>
2023-03-19 10:35:29 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function openReg(){
     document.getElementsByClassName(".container").className = "container2";
 }
-
 let button = document.querySelector('.profileBtn')
 button.addEventListener('click', openReg)
+
+
+$(document).ready(function() {
+    $('.slider').bxSlider({
+        infiniteLoop: true
+    });
+});
+
+
+let btn = document.querySelector('.BUY')
+btn.addEventListener('click', move)
+
+function move() {
+    
+}
